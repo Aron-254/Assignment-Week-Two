de71e83426e0898341387b4034a224ee0778a597
unknown <ngetichkipyegonaron@gmail.com>
2023-03-14 21:36:47 +0300

    Update

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,5 @@
+
+//Declaration of global variables
 const FEMALE_NAMES = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
 const MALE_NAMES = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
 let userAkanName = null;
@@ -6,6 +8,8 @@ let yy=null;
 let mm=null;
 let dd=null;
 let gender= null;
+
+// Function to get raw data from user
 
 function getUserInfo(){
   let birthYear = prompt("Enter your birth year");
@@ -16,12 +20,16 @@ function getUserInfo(){
   gender = prompt("Enter gender");
 }
 
+// Formula to calculate the day from raw data provided
+
 function calculateDayIndex(cc, yy, mm, dd) {
   let indexOfDay =Math.floor(
     (cc/4-2*cc -2+ (5*yy) / 4 + (26 * (mm + 1))/10 + dd)%7
   );
   return indexOfDay;
 }
+
+// Function to generate Akan Name
 
 function getAkanName(indexOfDay) {
   let dayIndex= calculateDayIndex(cc,yy,mm,dd
@@ -34,6 +42,9 @@ function getAkanName(indexOfDay) {
     console.log("Gender is not known");
   }
 }
+
+// Main function to combine all functions.
+
 function main(){
   let dayIndex=getUserInfo();
   getAkanName(dayIndex);
